fd741a08b023063298f0460f9e78c2f273c7c06f
joaocorreia23 <j.correia2213@gmail.com>
2019-10-28 19:27:43 +0000

    Primeira Alteração

--- src/main/java/BookRunner.java
+++ src/main/java/BookRunner.java
@@ -1,7 +1,22 @@
+import models.Book;
+
 public class BookRunner {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Exercicio1");
+
+        Book book1 = new Book();
+        book1.setTitulo("The Art Of Computer Programming");
+        Book book2 = new Book();
+        book2.setTitulo("Effective Java");
+        Book book3 = new Book();
+        book3.setTitulo("Clean Code");
+
+
+        System.out.println(String.format(  "Name : %s", book1.getTitulo()));
+        System.out.println(String.format( "Name : %s", book2.getTitulo()));
+        System.out.println(String.format( "Name : %s", book3.getTitulo()));
     }
 }
+
--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,5 +1,15 @@
 package models;
 
 public class Book {
+    private String titulo;
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 }
+
+
